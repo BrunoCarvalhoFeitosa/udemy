9a5777c33eaa5927a30c93ba431f39a269f83f18
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2024-10-08 18:42:51 -0300

    fix: fixing auth layout width

--- app/(pages)/(auth)/layout.tsx
+++ app/(pages)/(auth)/layout.tsx
@@ -11,7 +11,7 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
             <HeaderMobile />
             <main className="flex justify-center items-center w-full">
                 <div className="px-0 md:px-8 flex items-center mx-auto w-full h-full">
-                    <div className="mx-auto py-8 lg:py-16 lg:px-10 flex flex-col justify-center w-full xl:w-2/3 h-full">
+                    <div className="mx-auto py-8 lg:py-16 lg:px-10 flex flex-col justify-center w-full xl:w-[72%] h-full">
                         <div className="w-full">
                             {children}
                         </div>
